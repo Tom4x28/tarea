98f9fdc8961a1481841a99e3d6036602a8085ecb
Tom4x28 <tomascorujo2008@gmail.com>
2024-08-24 17:38:03 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
-            "ROFL": "Una respuesta a una broma"
-            "SHEESH": "Ligera desaprobación"
-            "CREEPY": "Aterrador, siniestro"
-            "AGGRO": "Ponerse agresivo/enojado"
+            "ROFL": "Una respuesta a una broma",
+            "SHEESH": "Ligera desaprobación",
+            "CREEPY": "Aterrador, siniestro",
+            "AGGRO": "Ponerse agresivo/enojado",
             }
             
 word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
 
 if word in meme_dict.keys():
-    print(meme_dict[word])
+    print("El significado de la palabra es: ", meme_dict[word])
 else:
     print("Lo sentimos no tenemos el significado de esa palabra tienes alguna otra?")
